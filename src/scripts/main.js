1fde3863b2964d92fe0261b50793d5383c344a36
Anubhav VISHWAKARMA <anubhav.vishwakarma@amadeus.com>
2017-07-11 23:23:20 +0530

    lele

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,53 @@
-var a;
+(function(window, document, $){
+	
+	'use strict';
+
+	var hoteList = [
+		{
+			city: 'tokyo',
+			hotel: 'HOTEL MYSTAYS PREMIER',
+			address: '',
+			img: ''
+		},
+		{
+			city: 'Kyoto',
+			hotel: 'HOTEL MYSTAYS Kyoto',
+			address: '',
+			img: ''
+		},
+		{
+			city: 'Osaka',
+			hotel: 'HOTEL MYSTAYS Osaka',
+			address: '',
+			img: ''
+		},
+		{
+			city: 'Kanazawa',
+			hotel: 'HOTEL MYSTAYS Kanazawa',
+			address: '',
+			img: ''
+		}
+	];
+
+	window.myStays = {};
+
+	var options = '<option value="default">Please Select a hotel</option>';
+
+	myStays.init = function() {
+		console.log(123)
+		for (var i = 0; i < hoteList.length; i++) {
+			options += '<option value="'+ hoteList[i].city +'">' + hoteList[i].hotel + '</option>';
+		}
+		$('#hotelName').html(options);
+		$( ".js-datepicker" ).datepicker();
+	};
+
+	$('#main-form-btn').on('click', function(){
+		console.log($('#mainForm').serializeArray());
+		console.log($('#mainForm').serialize());
+
+	});
+
+	$(document).ready(myStays.init);
+
+})(window, document, jQuery);

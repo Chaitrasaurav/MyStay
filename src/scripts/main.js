116be9b17b03a84f2079c12023843b40f720b172
Anubhav VISHWAKARMA <anubhav.vishwakarma@amadeus.com>
2017-07-12 23:14:10 +0530

    main form logic & slider WIP

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,51 +2,197 @@
 	
 	'use strict';
 
-	var hoteList = [
-		{
-			city: 'tokyo',
-			hotel: 'HOTEL MYSTAYS PREMIER',
-			address: '',
-			img: ''
+	var cityHotelMap = {
+		'tokyo': {
+			city: 'Tokyo',
+			id: 'tokyo',
+			hotels: [
+				{
+					name: 'HOTEL MYSTAYS PREMIER1',
+					address: '',
+					img: '',
+					id: 'jptok26324'
+				},
+				{
+					name: 'HOTEL MYSTAYS PREMIER2',
+					address: '',
+					img: '',
+					id: 'jptok263242'
+				},
+				{
+					name: 'HOTEL MYSTAYS PREMIER3',
+					address: '',
+					img: '',
+					id: 'jptok263243'
+				},
+				{
+					name: 'HOTEL MYSTAYS PREMIER4',
+					address: '',
+					img: '',
+					id: 'jptok263244'
+				}
+			]
 		},
-		{
+		'kyoto':{
 			city: 'Kyoto',
-			hotel: 'HOTEL MYSTAYS Kyoto',
-			address: '',
-			img: ''
+			id: 'kyoto',
+			hotels: [
+				{
+					name: 'HOTEL MYSTAYS Kyoto',
+					address: '',
+					img: '',
+					id: 'jptok26324'
+				},
+				{
+					name: 'HOTEL MYSTAYS Kyoto1',
+					address: '',
+					img: '',
+					id: 'jptok26324Kyoto1'
+				},
+				{
+					name: 'HOTEL MYSTAYS Kyoto2',
+					address: '',
+					img: '',
+					id: 'jptok26324Kyoto2'
+				},
+				{
+					name: 'HOTEL MYSTAYS Kyoto3',
+					address: '',
+					img: '',
+					id: 'jptok26324Kyoto3'
+				}
+			]
 		},
-		{
+		'osaka':{
 			city: 'Osaka',
-			hotel: 'HOTEL MYSTAYS Osaka',
-			address: '',
-			img: ''
-		},
-		{
-			city: 'Kanazawa',
-			hotel: 'HOTEL MYSTAYS Kanazawa',
-			address: '',
-			img: ''
+			id: 'osaka',
+			hotels: [
+				{
+					name: 'HOTEL MYSTAYS Osaka',
+					address: '',
+					img: '',
+					id: 'jptok26324Osaka'
+				},
+				{
+					name: 'HOTEL MYSTAYS Osaka1',
+					address: '',
+					img: '',
+					id: 'jptok26324Osaka1'
+				},
+				{
+					name: 'HOTEL MYSTAYS Osaka2',
+					address: '',
+					img: '',
+					id: 'jptok26324Osaka2'
+				},
+				{
+					name: 'HOTEL MYSTAYS Osaka3',
+					address: '',
+					img: '',
+					id: 'jptok26324Osaka3'
+				}
+			]
 		}
-	];
+	};
 
 	window.myStays = {};
 
-	var options = '<option value="default">Please Select a hotel</option>';
+	var options = '<option value="">Please Select a Hotel</option>';
 
 	myStays.init = function() {
-		console.log(123)
-		for (var i = 0; i < hoteList.length; i++) {
-			options += '<option value="'+ hoteList[i].city +'">' + hoteList[i].hotel + '</option>';
-		}
-		$('#hotelName').html(options);
-		$( ".js-datepicker" ).datepicker();
+		$( ".js-datepicker.check-in" ).datepicker({
+			dateFormat: "mm/dd/yy",
+			minDate: new Date(),
+			onSelect: function(dateText, inst) {
+				var date2 = $( ".js-datepicker.check-in" ).datepicker('getDate');
+		            date2.setDate(date2.getDate() + 1);
+		            $( ".js-datepicker.check-out" ).datepicker('setDate', date2);
+		            //sets minDate to dt1 date + 1
+		            $( ".js-datepicker.check-out" ).datepicker('option', 'minDate', date2);
+
+		        //$( ".js-datepicker.check-out" ).datepicker("option","minDate", $( ".js-datepicker.check-in" ).datepicker("getDate"));
+		    }
+		});
+		$( ".js-datepicker.check-out" ).datepicker({
+			dateFormat: "mm/dd/yy",
+		});
+
+		// var hotelsSelected = cityHotelMap[selectedCity].hotels;
+
+		for(var city in cityHotelMap) {
+			console.log(cityHotelMap[city].hotels);
+				var hotelsSelected = cityHotelMap[city].hotels;
+				for (var i = 0; i < hotelsSelected.length; i++) {
+					options += '<option value="'+ hotelsSelected[i].id +'">' + hotelsSelected[i].name + '</option>';
+				}
+				$('.js-hotel').html(options);
+		};
+
+		$('.lazy').slick({
+		  lazyLoad: 'ondemand',
+		  slidesToShow: 4,
+		  slidesToScroll: 1
+		});	
 	};
 
 	$('#main-form-btn').on('click', function(){
-		console.log($('#mainForm').serializeArray());
-		console.log($('#mainForm').serialize());
+		var $this = $(this),
+			$form = $this.parents("form"),
+			isValid = true;
 
+		$form.find(':input.required').each(function(index){
+		    console.log(this.value)
+		    if(!this.value.length) {
+		    	isValid = false;
+		    	$(this).addClass('js-is-invalid');
+		    }else {
+		    	$(this).removeClass('js-is-invalid');
+		    }
+		});
+
+		if(isValid) {
+			console.log($('#mainForm').serializeArray());
+			console.log($('#mainForm').serialize());
+			var queryParams = "s=results";
+			$form.serializeArray().forEach(function(value){
+				console.log(value)
+				queryParams +=  '&' + value.name + '=' + value.value;
+			});
+			queryParams += '&children1=0&rooms=1&code=5&locale=en_GB&currency=JPY&stid=2hj7fselc&=undefined'
+			var url = 'https://www.book-secure.com/index.php?' + queryParams;
+			var win = window.open(url, '_blank');
+  			win.focus();
+		}
+		//https://www.book-secure.com/index.php?
+		//s=results
+		//&property=jptok26324
+		//&arrival=2017-07-12
+		//&departure=2017-07-13]
+		//&adults1=2
+		//&children1=0
+		//&rooms=1
+		//&code=5
+		//&locale=en_GB
+		//&currency=JPY
+		//&stid=2hj7fselc
+		//&=undefined
 	});
+
+	$( ".js-city" ).on('change', function(e) {
+		var selectedCity = e.target.value,
+			hotelsSelected = cityHotelMap[selectedCity].hotels,
+			options = '<option value="default">Please Select a Hotel</option>';
+		if(hotelsSelected.length){
+			for (var i = 0; i < hotelsSelected.length; i++) {
+				options += '<option value="'+ hotelsSelected[i].id +'">' + hotelsSelected[i].name + '</option>';
+			}
+			$('.js-hotel').empty().html(options);
+		}
+	});
+
+	$('.js-check-box').change(function() {
+        $('.js-main-form-btn').attr("disabled", !$(this).is(":checked"));
+    });
 
 	$(document).ready(myStays.init);
 

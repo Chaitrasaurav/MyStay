a55c8af32ebfbe9ae3d63e576d8e0102b0a8c8d2
BOSTON\chaitra.naik <chaitra.naik@ef.com>
2017-07-14 17:41:33 +0530

    Lightbox functionality added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -326,3 +326,21 @@
 	});
 
 })(window, document, jQuery);
+
+window.document.onkeydown = function (e) {
+	    if (!e){
+	        e = event;
+	    }
+	    if (e.keyCode == 27){
+	        lightbox_close();
+	    }
+	}
+	function lightboxOpen(){
+	    window.scrollTo(0,0);
+	    document.getElementById('light').style.display='block';
+	    document.getElementById('fade').style.display='block';  
+	}
+	function lightboxClose(){
+	    document.getElementById('light').style.display='none';
+	    document.getElementById('fade').style.display='none';
+	}
